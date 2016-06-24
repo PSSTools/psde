6224af645ce090f528a84be5f673089a1da55e3a
Matthew Ballance <matt.ballance@gmail.com>
2016-06-24 13:54:57 -0700

    More progress getting DMA chaining example through the PSS elaborator

--- psstools/plugins/net.sf.psstools.lang/src/net/sf/psstools/lang/pss2xml/Elaborator.java
+++ psstools/plugins/net.sf.psstools.lang/src/net/sf/psstools/lang/pss2xml/Elaborator.java
@@ -9,6 +9,7 @@ import org.eclipse.emf.ecore.EObject;
 import net.sf.psstools.lang.pSS.Model;
 import net.sf.psstools.lang.pSS.action_body_item;
 import net.sf.psstools.lang.pSS.action_declaration;
+import net.sf.psstools.lang.pSS.action_field_declaration;
 import net.sf.psstools.lang.pSS.component_body_item;
 import net.sf.psstools.lang.pSS.component_declaration;
 import net.sf.psstools.lang.pSS.constraint_declaration;
@@ -18,8 +19,15 @@ import net.sf.psstools.lang.pSS.dec_number;
 import net.sf.psstools.lang.pSS.enum_declaration;
 import net.sf.psstools.lang.pSS.enum_item;
 import net.sf.psstools.lang.pSS.expression;
+import net.sf.psstools.lang.pSS.graph_declaration;
+import net.sf.psstools.lang.pSS.graph_labeled_production;
+import net.sf.psstools.lang.pSS.graph_parallel_stmt;
+import net.sf.psstools.lang.pSS.graph_production;
+import net.sf.psstools.lang.pSS.graph_repeat_stmt;
+import net.sf.psstools.lang.pSS.graph_sequence_stmt;
 import net.sf.psstools.lang.pSS.hex_number;
 import net.sf.psstools.lang.pSS.hierarchical_id;
+import net.sf.psstools.lang.pSS.import_stmt;
 import net.sf.psstools.lang.pSS.integer_type;
 import net.sf.psstools.lang.pSS.logical_equality_expr;
 import net.sf.psstools.lang.pSS.logical_inequality_expr;
@@ -30,10 +38,13 @@ import net.sf.psstools.lang.pSS.open_range_value;
 import net.sf.psstools.lang.pSS.package_body_item;
 import net.sf.psstools.lang.pSS.package_declaration;
 import net.sf.psstools.lang.pSS.package_identifier;
+import net.sf.psstools.lang.pSS.package_import_pattern;
 import net.sf.psstools.lang.pSS.struct_body_item;
 import net.sf.psstools.lang.pSS.struct_declaration;
 import net.sf.psstools.lang.pSS.struct_field_declaration;
 import net.sf.psstools.lang.pSS.struct_field_modifier;
+import net.sf.psstools.lang.pSS.type_identifier;
+import net.sf.psstools.lang.pSS.user_defined_type;
 import net.sf.psstools.lang.pSS.variable_ref;
 
 public class Elaborator {
@@ -85,6 +96,10 @@ public class Elaborator {
 		for (action_body_item it : a.getBody()) {
 			if (it instanceof constraint_declaration) {
 				elaborate_constraint((constraint_declaration)it);
+			} else if (it instanceof action_field_declaration) {
+				elaborate_action_field((action_field_declaration)it);
+			} else if (it instanceof graph_declaration) {
+				elaborate_graph((graph_declaration)it);
 			} else {
 				println("<unknown_item class=\"" + it.getClass() + "\"/>");
 			}
@@ -92,6 +107,30 @@ public class Elaborator {
 		
 		dec_indent();
 		println("</action>");
+	}
+	
+	private void elaborate_action_field(action_field_declaration f) {
+		data_declaration decl = f.getDeclaration();
+		
+		for (data_instantiation inst : decl.getInstances()) {
+			String tag = "<field name=\"" + inst.getName() + "\"";
+		
+			if (f instanceof struct_field_modifier) {
+				struct_field_modifier m = (struct_field_modifier)f;
+				if (m.isRand()) {
+					tag += " type=\"rand\"";
+				}
+			}
+			
+			tag += ">";
+			println(tag);
+			
+			inc_indent();
+			elaborate_type(decl.getType());
+			dec_indent();
+			
+			println("</field>");
+		}		
 	}
 	
 	private void elaborate_component(component_declaration c) {
@@ -110,6 +149,8 @@ public class Elaborator {
 				elaborate_action((action_declaration)it);
 			} else if (it instanceof struct_declaration) {
 				elaborate_struct((struct_declaration)it);
+			} else if (it instanceof import_stmt) {
+				elaborate_import((import_stmt)it);
 			} else {
 				println("<unknown_item class=\"" + it.getClass() + "\"/>");
 			}
@@ -271,6 +312,120 @@ public class Elaborator {
 		println("</enum>");
 	}
 	
+	private void elaborate_graph(graph_declaration graph) {
+		println("<graph>");
+		inc_indent();
+	
+		elaborate_graph_production_list(graph.getBody());
+		
+		dec_indent();
+		println("</graph>");
+	}
+	
+	private void elaborate_graph_production_list(EList<graph_production> productions) {
+		for (graph_production p : productions) {
+			elaborate_graph_production(p);
+		}		
+	}
+	
+	private void elaborate_graph_production(graph_production p) {
+		if (p instanceof graph_repeat_stmt) {
+			elaborate_graph_repeat((graph_repeat_stmt)p);
+		} else if (p instanceof graph_parallel_stmt) {
+			elaborate_graph_parallel((graph_parallel_stmt)p);
+		} else if (p instanceof graph_sequence_stmt) {
+			elaborate_graph_sequence((graph_sequence_stmt)p);
+		} else {
+			println("<unknown_item class=\"" + p.getClass() + "\"/>");
+		}		
+	}
+	
+	private void elaborate_graph_parallel(graph_parallel_stmt p) {
+		println("<parallel>");
+		inc_indent();
+
+		for (graph_labeled_production lp : p.getProductions()) {
+			elaborate_graph_production(lp.getProduction());
+		}
+		
+		dec_indent();
+		println("</parallel>");
+	}
+	
+	private void elaborate_graph_repeat(graph_repeat_stmt r) {
+		String tag = "<repeat type=\"";
+		expression expr = r.getExpr();
+	
+		if (r.isCount()) {
+			String ivar = r.getIvar();
+			tag += "count\"";
+			
+			if (ivar != null) {
+				tag += " itvar=\"" + ivar + "\"";
+			}
+		} else if (r.isPre_cond()) {
+			tag += "while\"";
+		} else if (r.isPost_cond()) {
+			tag += "until\"";
+		} else {
+			tag += "<UNKNOWN>\"";
+		}
+		
+		tag += "/>";
+		println(tag);
+		inc_indent();
+		
+		if (expr != null) {
+			elaborate_expr(expr);
+		}
+		
+		elaborate_graph_production_list(r.getBody().getStmt_list());
+		
+		dec_indent();
+		println("</repeat>");
+	}
+	
+	private void elaborate_graph_sequence(graph_sequence_stmt s) {
+		if (s.getItems().size() > 0) {
+			println("<sequential>");
+			inc_indent();
+			for (String it : s.getItems()) {
+				println("<traverse action=\"" + it + "\"/>");
+			}
+			dec_indent();
+			println("</sequential>");
+		} else {
+			if (s.getType() != null) {
+				println("<unknown_item type=\"inline action declaration\"/>");
+			} else {
+				String tag = "<travserse action=\"" + s.getItem() + "\"";
+				
+				if (s.getInline_with() == null) {
+					tag += "/>";
+					println(tag);
+				} else {
+					tag += ">";
+					println(tag);
+					inc_indent();
+					println("<unknown_item type=\"inline with constraint\"/>");
+					dec_indent();
+					println("</traverse>");
+				}
+			}
+		}
+	}
+	
+	private void elaborate_import(import_stmt imp) {
+		package_import_pattern p = imp.getPkg();
+		String imp_s = tid2string(p.getPackage());
+		
+		if (p.isWildcard()) {
+			imp_s += "::*";
+		}
+		
+		println("<import package=\"" + imp_s + "\"/>");
+	}
+	
 	private void elaborate_package(package_declaration p) {
 		String tag = "<package name=\"" + hid2string(p.getName()) + "\">";
 		println(tag);
@@ -281,6 +436,8 @@ public class Elaborator {
 				elaborate_struct((struct_declaration)it);
 			} else if (it instanceof enum_declaration) {
 				elaborate_enum((enum_declaration)it);
+			} else if (it instanceof import_stmt) {
+				elaborate_import((import_stmt)it);
 			} else {
 				println("<unknown_item class=\"" + it.getClass() + "\"/>");
 			}
@@ -367,6 +524,9 @@ public class Elaborator {
 			
 			tag += "/>";
 			println(tag);
+		} else if (type instanceof user_defined_type) {
+			user_defined_type udt = (user_defined_type)type;
+			println("<user-defined name=\"" + tid2string(udt.getTypename()) + "\"/>");
 		} else {
 			println("<unknown_item class=\"" + type.getClass()+ "\"/>");
 		}
@@ -389,7 +549,7 @@ public class Elaborator {
 		}
 	}
 	
-	private String hid2string(package_identifier id) {
+	private static String hid2string(package_identifier id) {
 		if (id instanceof hierarchical_id) {
 			return hid2string((hierarchical_id)id);
 		} else {
@@ -397,9 +557,22 @@ public class Elaborator {
 		}
 	}
 	
-	private String hid2string(hierarchical_id id) {
+	private static String hid2string(hierarchical_id id) {
 		String ret = id.getPath().get(0);
 		
+		return ret;
+	}
+	
+	private static String tid2string(type_identifier id) {
+		String ret = "";
+		
+		for (int i=0; i<id.getElems().size(); i++) {
+			if (ret.length() > 0) {
+				ret += "::";
+			}
+			ret += id.getElems().get(i);
+		}
+
 		return ret;
 	}
 	
