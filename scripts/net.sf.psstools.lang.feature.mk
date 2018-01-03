07183dd57051b5ce4d4520c03383ed965e18b2f0
Matthew Ballance <matt.ballance@gmail.com>
2018-01-03 13:43:56 -0800

    Update for initial release

--- scripts/net.sf.psstools.lang.feature.mk
+++ scripts/net.sf.psstools.lang.feature.mk
@@ -16,6 +16,7 @@ $(FEATURE)_mw2e :
 	$(Q)$(ECLIPSE_ANT) \
 		-buildfile $(call NATIVE_PATH,$(PSSTOOLS_SCRIPTS_DIR)/mw2e_net.sf.psstools.lang.feature.xml) \
 		-DbuildDirectory=$(call NATIVE_PATH,$(net.sf.psstools.lang.feature_BUILDDIR)/build) \
+		-Dfeature.version=$(FEATURE_VERSION) \
 		-Dsrcdir=$(call NATIVE_PATH,$(PSSTOOLS_DIR)/psstools) \
 		-verbose \
 		build
