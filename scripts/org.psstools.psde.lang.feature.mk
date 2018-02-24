0eb8dd1a2a2c8a0b3a34f152737697b17a9369ad
Matthew Ballance <matt.ballance@gmail.com>
2018-02-24 15:23:28 -0800

    Updates for the 0.0.3 release

--- scripts/org.psstools.psde.lang.feature.mk
+++ scripts/org.psstools.psde.lang.feature.mk
@@ -1,8 +1,8 @@
 
-FEATURE := net.sf.psstools.lang.feature
-FEATURE_SRCDIR := $(PSSTOOLS_DIR)/psstools
+FEATURE := org.psstools.psde.lang.feature
+FEATURE_SRCDIR := $(PSSTOOLS_DIR)/psde
 FEATURE_DIR := $(FEATURE_SRCDIR)/features/$(FEATURE)
-FEATURE_VERSION := 0.0.3
+# FEATURE_VERSION := 0.0.1
 FEATURE_PRE_BUILD_TARGETS := $(FEATURE)_mw2e
 
 
@@ -14,10 +14,10 @@ ECLIPSE_SDK_PKGS += xtext
 else # Rules
 $(FEATURE)_mw2e : 
 	$(Q)$(ECLIPSE_ANT) \
-		-buildfile $(call NATIVE_PATH,$(PSSTOOLS_SCRIPTS_DIR)/mw2e_net.sf.psstools.lang.feature.xml) \
-		-DbuildDirectory=$(call NATIVE_PATH,$(net.sf.psstools.lang.feature_BUILDDIR)/build) \
+		-buildfile $(call NATIVE_PATH,$(PSSTOOLS_SCRIPTS_DIR)/mw2e_org.psstools.psde.lang.feature.xml) \
+		-DbuildDirectory=$(call NATIVE_PATH,$(org.psstools.psde.lang.feature_BUILDDIR)/build) \
 		-Dfeature.version=$(FEATURE_VERSION) \
-		-Dsrcdir=$(call NATIVE_PATH,$(PSSTOOLS_DIR)/psstools) \
+		-Dsrcdir=$(call NATIVE_PATH,$(PSSTOOLS_DIR)/psde) \
 		-Dos=$(osgi_os) -Dws=$(osgi_ws) -Darch=$(osgi_arch) \
 		-verbose \
 		build
--- scripts/psde.mk
+++ scripts/psde.mk
@@ -1,22 +1,15 @@
 
+FEATURE := org.psstools.psde.lang.feature
 
-PRODUCT := psde
-PRODUCT_FILE := /net.sf.psstools.pssc.feature/pssc.product
-PRODUCT_SRCDIR := $(PSSTOOLS_DIR)/psstools
-PRODUCT_FEATURE_DIR := $(PSSTOOLS_DIR)/psstools/features/net.sf.psstools.pssc.feature
-PRODUCT_VERSION := 0.0.1
-# PRODUCT_PRE_BUILD_TARGETS += $(PRODUCT)pssc_mw2e
 
 include $(ECLIPSESCRIPTS_DIR)/mkfiles/eclipseproduct.mk
 
 ifneq (1,$(RULES))
 ECLIPSE_SDK_PKGS += xtext
-
 else # Rules
-
-pssc_mw2e : 
+$(PRODUCT)_pssc_mw2e : 
 	$(Q)$(ECLIPSE_ANT) -buildfile $(call NATIVE_PATH,$(PSSTOOLS_SCRIPTS_DIR)/mw2e_build.xml) \
 		-DbuildDirectory=$(call NATIVE_PATH,$($(PRODUCT)_BUILDDIR)/product) build
-		
+
 endif
 
